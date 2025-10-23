0341f873ff56c7d078ae0b87dabaefd01448152b
ErfanKarbalaei <Erfan.karbalaii77@gmail.com>
2025-10-23 22:36:37 +0330

    change table structure for user, transaction, card add seed for create fake data

--- app/db/models/card_model.py
+++ app/db/models/card_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean, DateTime, func
 from sqlalchemy.orm import relationship
 from app.db.session import Base
 
@@ -7,8 +7,12 @@ class Card(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     card_number = Column(String(16), unique=True, nullable=False)
+    cvv2 = Column(String(4), nullable=False)
+    expire_date = Column(String(5), nullable=False)
     balance = Column(Numeric(15, 2), default=0)
+    is_active = Column(Boolean, default=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     owner = relationship("User", back_populates="cards")
     transactions_from = relationship("Transaction", foreign_keys="[Transaction.source_card_id]", back_populates="source_card")
