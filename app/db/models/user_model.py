05f995b3fefb7828f2bd384a9da3d601b91a6521
ErfanKarbalaei <Erfan.karbalaii77@gmail.com>
2025-10-25 00:17:20 +0330

    fix : correct import

--- app/db/models/user_model.py
+++ app/db/models/user_model.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Date, Boolean, DateTime, func
 from sqlalchemy.orm import relationship
 from app.db.session import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
